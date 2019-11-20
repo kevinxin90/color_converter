d54ffd66dcee2690d7724a6b26d9961672a319de
jiwen xin <kevinxin@scripps.edu>
2019-11-20 13:58:59 -0800

    update test case

--- test/converter.js
+++ test/converter.js
@@ -21,9 +21,9 @@ describe("Color Code Converter", function() {
             var green = converter.hexToRgb("00ff00");
             var blue  = converter.hexToRgb("0000ff");
 
-            expect(red).to.deep.equal([255, 0, 0]);
-            expect(green).to.deep.equal([0, 255, 0]);
-            expect(blue).to.deep.equal([0, 0, 255]);
+            expect(red).to.deep.equal([255,0,0]);
+            expect(green).to.deep.equal([0,255,0]);
+            expect(blue).to.deep.equal([0,0,255]);
         });
     });
 })
